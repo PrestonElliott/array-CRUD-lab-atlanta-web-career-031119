03ac6ad7a519bfcd6213fa7941d5e7d3462b8bf8
Preston Elliott <preston.elliott24@gmail.com>
2019-03-02 23:06:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["Lola", "Lucy", "Magic", "Patches"]
+  new_array = ["Lola", "Lucy", "Magic", "Patches"]
 end
 
 def add_element_to_end_of_array(array, element)
