ac8440814372ce326d81c65ed27d7409505ffaf9
Preston Elliott <preston.elliott24@gmail.com>
2019-03-02 22:59:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,12 @@ def create_an_empty_array
 end
 
 def create_an_array
+  create_an_empty_array
   array = ["Lola", "Lucy", "Magic", "Patches"]
 end
 
 def add_element_to_end_of_array(array, element)
-  array << element = "Dante"
+  create_an_array << "Dante"
 end
 
 def add_element_to_start_of_array(array, element)
