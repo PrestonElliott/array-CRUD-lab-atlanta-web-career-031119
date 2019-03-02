36c42a22c50074f883d2b7f7dccface92991248f
Preston Elliott <preston.elliott24@gmail.com>
2019-03-02 22:51:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  << "arrays!"
+  return create_an_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
